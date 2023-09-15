f4b9871550cf680c8da89d207f5cf5220a414556
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2023-09-15 11:19:51 +0530

    Update Chandrayan3TDDAssessment.java

--- Chandrayan3TDDAssessment.java
+++ Chandrayan3TDDAssessment.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Chandrayan3TDDAssessment
 {
 	int x, y, z;
-	String direction;
+	String direction, up_down = "s";
 	
 	public static void main(String[] args)
 	{
@@ -19,14 +19,105 @@ public class Chandrayan3TDDAssessment
 		System.out.print("z> ");
 		obj.z = sc.nextInt();
 		
-		System.out.println("Enter the initial direction:- ");
+		System.out.print("Enter the initial direction:- ");
 		obj.direction = sc.next();
 		
-		System.out.print("Enter the commands(separated by space):- ");
-		String [] str = sc.nextLine().split(" ");
+		System.out.print("Enter the commands:- ");
+		String [] str = sc.next().split("");
 		
-		
-//		for(String s:str)
-//			System.out.println(s);
+		System.out.println("\nInitial direction: " + obj.direction);
+		for(String s:str)
+		{
+			switch(s)
+			{
+			case "f":
+				if(obj.up_down == "u" || obj.up_down == "d")
+				{
+					if(obj.up_down == "u")
+						obj.z++;
+					else
+						obj.z--;
+					System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.up_down);
+				}
+				else
+				{
+					switch(obj.direction)
+					{
+					case "N":
+						obj.y++;
+						break;
+						
+					case "E":
+						obj.x++;
+						break;
+						
+					case "S":
+						obj.y--;
+						break;
+						
+					case "W":
+						obj.x--;
+						break;
+					}
+					System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.direction);
+				}
+				break;
+				
+			case "b":
+				if(obj.up_down == "u" || obj.up_down == "d")
+				{
+					if(obj.up_down == "u")
+						obj.z--;
+					else
+						obj.z++;
+					System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.up_down);
+				}
+				else
+				{
+					switch(obj.direction)
+					{
+					case "N":
+						obj.y--;
+						break;
+						
+					case "E":
+						obj.x--;
+						break;
+						
+					case "S":
+						obj.y++;
+						break;
+						
+					case "W":
+						obj.x++;
+						break;
+					}
+					System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.direction);
+				}
+				break;
+				
+			case "l":
+				obj.up_down = "s";
+				switch(obj.direction)
+				{
+				case
+				}
+				break;
+				
+			case "r":
+				break;
+				
+			case "u":
+				break;
+				
+			case "d":
+				break;
+				
+			default:
+				System.out.println("Invalid command! '" + s + "'");
+				break;
+					
+			}
+		}
 	}
 }
