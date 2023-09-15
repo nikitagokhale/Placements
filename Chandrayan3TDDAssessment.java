77b74c2d9b5602b6ba7b0437356cf69fe245c05e
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2023-09-15 09:44:15 +0530

    Update Chandrayan3TDDAssessment.java

--- Chandrayan3TDDAssessment.java
+++ Chandrayan3TDDAssessment.java
@@ -1,13 +1,32 @@
+import java.util.*;
 
-public class Chandrayan3TDDAssessment {
-
-	public Chandrayan3TDDAssessment() {
-		// TODO Auto-generated constructor stub
+public class Chandrayan3TDDAssessment
+{
+	int x, y, z;
+	String direction;
+	
+	public static void main(String[] args)
+	{
+		Chandrayan3TDDAssessment obj = new Chandrayan3TDDAssessment();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the initial coordinates:");
+		System.out.print("x> ");
+		obj.x = sc.nextInt();
+		System.out.print("y> ");
+		obj.y = sc.nextInt();
+		System.out.print("z> ");
+		obj.z = sc.nextInt();
+		
+		System.out.println("Enter the initial direction:- ");
+		obj.direction = sc.next();
+		
+		System.out.print("Enter the commands(separated by space):- ");
+		String [] str = sc.nextLine().split(" ");
+		
+		
+//		for(String s:str)
+//			System.out.println(s);
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
