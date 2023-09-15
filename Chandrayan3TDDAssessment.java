27a16d3667dd6539f2ba09276ad6b5c54b5426e5
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2023-09-15 11:25:29 +0530

    Update Chandrayan3TDDAssessment.java

--- Chandrayan3TDDAssessment.java
+++ Chandrayan3TDDAssessment.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Chandrayan3TDDAssessment
 {
 	int x, y, z;
-	String direction, up_down = "s";
+	String direction, up_down = "S";
 	
 	public static void main(String[] args)
 	{
@@ -19,13 +19,17 @@ public class Chandrayan3TDDAssessment
 		System.out.print("z> ");
 		obj.z = sc.nextInt();
 		
-		System.out.print("Enter the initial direction:- ");
+		System.out.print("Enter the initial direction (N/S/E/W):- ");
 		obj.direction = sc.next();
 		
-		System.out.print("Enter the commands:- ");
-		String [] str = sc.next().split("");
+		System.out.print("\nEnter the commands (Separated by comma):- ");
+		String [] str = sc.next().split(",");
+
+		System.out.println("\n--------------------------------------------------\nInitial Position: " + "(" + obj.x + ", " + obj.y + ", " + obj.z + ")");
+		 
+		System.out.println("\nInitial Direction: " + obj.direction + "\n");
 		
-		System.out.println("\nInitial direction: " + obj.direction);
+		System.out.println("The course of the spacecraft is:");
 		for(String s:str)
 		{
 			switch(s)
@@ -116,6 +120,7 @@ public class Chandrayan3TDDAssessment
 					obj.direction = "N";
 					break;
 				}
+				System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.direction);
 				break;
 				
 			case "r":
@@ -138,15 +143,16 @@ public class Chandrayan3TDDAssessment
 					obj.direction = "S";
 					break;
 				}
+				System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.direction);
 				break;
 				
 			case "u":
-				obj.up_down = "u";
+				obj.up_down = "U";
 				System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.up_down);
 				break;
 				
 			case "d":
-				obj.up_down = "d";
+				obj.up_down = "D";
 				System.out.println(s + " - (" + obj.x + ", " + obj.y + ", " + obj.z + ") - " + obj.up_down);
 				break;
 				
@@ -156,5 +162,6 @@ public class Chandrayan3TDDAssessment
 					
 			}
 		}
+		System.out.println("\nThe final position of the spacecraft: (" + obj.x + "," + obj.y + "," + obj.z + ")\n--------------------------------------------------");
 	}
 }
